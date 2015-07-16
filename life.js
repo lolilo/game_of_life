64833b369016264d9af9d86cd3f2e65ef5de796e
Liana Lo <liana.lo@prezi.com>
2015-07-16 21:42:51 +0200

    neighbor (bad) logic

--- life.js
+++ life.js
@@ -12,10 +12,32 @@
     _.prototype = {
         next: function () {
             this.prevBoard = cloneArray(this.board)
+
+            for (var y=0; y<this.hegith; y++) {
+                for (var x=0; x<this.width; x++) {
+                    var neighbors = this.aliveNeighbors(this.prevBoard, x, y)
+                }
+            }
+        },
+
+        aliveNeighbors: function (array, x, y) {
+            var sum = 0;
+            var prevRow = array[y-1] || [];
+            var nextRow = array[y+1] || [];
+
+            var neighbors = [
+                array[y-1][x-1], array[y-1][x], array[y-1][x+1],
+                array[y][x-1], array[y][x+1],
+                array[y+1][x-1], array[y+1][x], array[y+1][x+1]
+            ].forEach(function (x) {
+                sum += +!!a; // convert to bool, then convert to num 0 or 1
+            });
+
+            return sum;
         },
 
         toString: function () {
-
+            return this.board.map(function (row) { return row.join(' '); }).join('\n');
         }
     };
 
@@ -29,14 +51,14 @@
 
 var game = new Life([
     [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
     [0, 0, 0, 0, 0]
 ]);
 
-console.log(game);
+console.log(game + '');
 
 game.next();
 
-console.log(game);
+console.log(game + '');
